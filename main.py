45f2cb8b39b4059d967cf4aa52cc23179b036df8
tinhtq <truongquangtinh1997@gmail.com>
2024-05-30 11:36:49 +0700

    add docker ignore

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from langchain_community.llms import Ollama
+from dotenv import load_dotenv
+import os
 
-llm = Ollama(model="llama3", base_url="http://localhost:11434")
+load_dotenv()
+
+llm = Ollama(model="llama3", base_url=os.getenv("BASEMODEL_URL"))
 
 resp = llm.invoke("Tell about the story")
 
